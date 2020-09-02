4d1ae67b11c8fa2428e40498ffa648b99a97c628
Lee Tsai <lee.x.tsai@gmail.com>
2020-09-02 19:11:10 -0400

    set up basic react components and prepare for CORS resolution

---
+++ client/src/PostCreate.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default () => {
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (event) => {
+    // prevent browser's default submit behavior
+    event.preventDefault();
+
+    // make a POST request to post service
+    await axios.post('http://localhost:4000/posts', {
+        title
+    });
+
+    setTitle('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
